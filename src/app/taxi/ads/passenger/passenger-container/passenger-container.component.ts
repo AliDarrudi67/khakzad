15bc1dceb8a642ae72ff15accb09de0dd116bdaa
darrudi <ali.daroudi@gmail.com>
2023-12-13 20:47:34 +0330

    prepare passenger form

--- src/app/taxi/ads/passenger/passenger-container/passenger-container.component.ts
+++ src/app/taxi/ads/passenger/passenger-container/passenger-container.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
+import * as moment from 'jalali-moment';
 
 @Component({
   selector: 'app-passenger-container',
@@ -6,8 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./passenger-container.component.scss']
 })
 export class PassengerContainerComponent {
+  form!: FormGroup
+  date = '';
 
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.groupForm()
+  }
+
+  groupForm() {
+    this.form = this.formBuilder.group({
+      name: ['', Validators.required],
+      type: ['passenger', Validators.required],
+      date: ['', Validators.required],
+      hour: ['', Validators.required],
+      minute: ['', Validators.required],
+      passengerCount: ['', Validators.required],
+      source: ['', Validators.required],
+      destination: ['', Validators.required],
+      mobile: ['', Validators.required]
+    })
+  }
   onDateChanged($event: any) {
+    console.log($event);
+    this.date = moment.from($event, 'fa').format('YYYY/MM/DD')
+  }
+
+  submitForm() {
+    console.log(this.form.value);
+    const data = this.form.value
+    data.startTime = this.date + ' ' + this.form.get('hour')?.value + ':' + this.form.get('minute')?.value
+    console.log(data);
 
   }
 }
--- src/app/taxi/ads/passenger/passenger.module.ts
+++ src/app/taxi/ads/passenger/passenger.module.ts
@@ -7,6 +7,7 @@ import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
 import {MatDatepickerModule} from "@angular/material/datepicker";
 import {MatSelectModule} from "@angular/material/select";
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import {MatSelectModule} from "@angular/material/select";
     MatInputModule,
     MatButtonModule,
     MatDatepickerModule,
-    MatSelectModule
+    MatSelectModule,
+    ReactiveFormsModule
   ]
 })
 export class PassengerModule { }
